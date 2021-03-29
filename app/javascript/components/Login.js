85722363dee401ca04023b04c86c97535547ea3a
Bret Kittle <bretkittle@gmail.com>
2021-03-29 09:18:30 -0700

    Setting up initial template project for building an API

---
+++ app/javascript/components/Login.js
@@ -0,0 +1,15 @@
+
+import React, { useState, useEffect } from 'react';
+import RInput from './RInput';
+
+function Login() {
+
+	return (
+		<div>
+			<RInput />
+		</div>
+	);
+
+}
+
+export default Login;
---
+++ app/javascript/components/RInput.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+function RInput() {
+
+	return(
+		<React.Fragment>
+			<label for="username">Username:</label>
+			<input type="text" />
+		</React.Fragment>
+	);
+
+}
+
+export default RInput;
